1c250b30bb524d79d7ceb27dd3bda95cec27bad1
Chris Wright <cawright@scu.edu>
2018-05-24 19:07:17 -0700

    Finished hw 5, and started hw7

---
+++ hw7/mlist.h
@@ -0,0 +1,18 @@
+//
+// Created by Chris Wright on 5/24/18.
+//
+
+#ifndef HW7_MLIST_H
+#define HW7_MLIST_H
+
+template <class T>
+class mlist
+{
+public:
+    bool empty
+
+private:
+
+};
+
+#endif //HW7_MLIST_H
